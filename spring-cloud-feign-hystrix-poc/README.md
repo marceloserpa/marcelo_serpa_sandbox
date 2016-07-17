4ded109d6f7b48d52c7c811ddee11514e2abe9df
MarceloIlegra <marcelo.serpa@ilegra.com>
2016-07-17 20:46:21 -0300

    add readme

---
+++ spring-cloud-feign-hystrix-poc/README.md
@@ -0,0 +1,29 @@
+About this sample
+====================
+
+This project is a sample using Feign client and Hystrix circuit breaker.
+
+First of all you need run the projects fake-api and feign-hystrix-client-app.
+
+Now you can test the service using the HTTP request sample: 
+
+**Add new product**
+<br />
+GET localhost:9000/client/products/
+
+{
+    "id": 1,
+    "name" : “My book sample",
+    "price" : 2
+}
+
+**List all product**
+<br />
+POST localhost:9000/client/products/
+
+Activating circuit breaker
+====================
+
+When the fake-api is down the circuit breakers are open.
+
+:)
--- spring-cloud-feign-hystrix-poc/feign-hystrix-client-app/src/main/java/com/marceloserpa/feignhystrix/ProductController.java
+++ spring-cloud-feign-hystrix-poc/feign-hystrix-client-app/src/main/java/com/marceloserpa/feignhystrix/ProductController.java
@@ -3,6 +3,8 @@ package com.marceloserpa.feignhystrix;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
 	@Autowired 
-	ProductClient service;
+	ProductClient client;
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public List<Product> list(){
-		return service.all();
+		return client.all();
+	}
+
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public Product getById(@PathVariable("id") Long id){
+		return client.getById(id);
 	}
 	
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public void save(@RequestBody Product product){
+		client.add(product);;
+	}	
 }
