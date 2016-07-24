bf7233799b7103a6dd57768355b5f20af64ae505
MarceloIlegra <marcelo.serpa@ilegra.com>
2016-07-24 15:09:17 -0300

    added HystrixDashboard

--- spring-cloud-feign-hystrix-poc/feign-hystrix-client-app/src/main/java/com/marceloserpa/feignhystrix/Application.java
+++ spring-cloud-feign-hystrix-poc/feign-hystrix-client-app/src/main/java/com/marceloserpa/feignhystrix/Application.java
@@ -1,14 +1,22 @@
 package com.marceloserpa.feignhystrix;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.metrics.servo.ServoMetricReader;
+import org.springframework.cloud.netflix.metrics.servo.ServoMetricServices;
+import org.springframework.cloud.netflix.metrics.servo.ServoMetricsAutoConfiguration;
 
-
-@RestController
+@EnableAutoConfiguration(exclude={ServoMetricsAutoConfiguration.class} )
 @EnableFeignClients
-@SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+@EnableHystrix
+@SpringBootApplication(exclude={ServoMetricServices.class, ServoMetricReader.class})
 public class Application {
 
 	public static void main(String[] args) {
