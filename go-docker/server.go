5b7de795e9160083b703e1a92c3394e214d7af87
marceloserpa <jdev.marcelo@gmail.com>
2018-02-16 16:32:36 -0200

    go: getting environment variable defined by docker build-args

--- go-docker/server.go
+++ go-docker/server.go
@@ -3,13 +3,15 @@ package main
 import (
     "fmt"
     "net/http"
+    "os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) {    
     fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
+    fmt.Println("PROFILE:", os.Getenv("PROFILE"))
     http.HandleFunc("/", handler)
     http.ListenAndServe(":8080", nil)
 }
