8cbda303b1cd0432f5f14319254e592120d08cab
marceloserpa <jdev.marcelo@gmail.com>
2018-03-16 15:43:13 -0300

    feat: added multicast to parallel processing

--- apache-camel-kafka-poc/src/main/java/com/marceloserpa/camelkafka/poc/healthcheckers/HttpHealthCheckRoute.java
+++ apache-camel-kafka-poc/src/main/java/com/marceloserpa/camelkafka/poc/healthcheckers/HttpHealthCheckRoute.java
@@ -5,7 +5,11 @@ import com.marceloserpa.camelkafka.poc.healthcheckers.models.Health;
 import com.marceloserpa.camelkafka.poc.healthcheckers.models.Metrics;
 import com.marceloserpa.camelkafka.poc.healthcheckers.parser.JsonParser;
 import com.marceloserpa.camelkafka.poc.healthcheckers.producer.ProducerHealthChecker;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.processor.aggregate.AggregationStrategy;
+
+import java.util.Objects;
 
 
 public class HttpHealthCheckRoute extends RouteBuilder{
@@ -13,8 +17,11 @@ public class HttpHealthCheckRoute extends RouteBuilder{
     @Override
     public void configure() throws Exception {
         from("jetty://http://localhost:8888/health")
-            .to("direct:health-check-consumer")
-            .to("direct:health-check-producer")
+            .multicast().parallelProcessing().aggregationStrategy(new HeadersAggregationStrategy())
+                .to("direct:health-check-consumer")
+                .to("direct:health-check-producer")
+            .end()
+            .log("end of multicast processing")
             .process(exchange -> {
                 Health health = new Health();
                 health.setKafkaConsumer(new Metrics(exchange.getIn().getHeader("consumer").toString()));
@@ -26,25 +33,37 @@ public class HttpHealthCheckRoute extends RouteBuilder{
                 }
                 exchange.getOut().setHeader("Content-Type", "application/json");
                 exchange.getOut().setBody(new JsonParser().parse(health));
-            })
 
+            })
         .end();
 
 
         from("direct:health-check-consumer")
+            .log("execute direct:health-check-consumer")
             .process(exchange -> {
-                exchange.getOut().setHeaders(exchange.getIn().getHeaders());
                 ConsumerHealthChecker consumerHealthChecker = new ConsumerHealthChecker();
                 exchange.getOut().setHeader("consumer", consumerHealthChecker.check() ? "UP" : "DOWN");
             })
         .end();
 
         from("direct:health-check-producer")
+            .log("execute direct:health-check-producer")
             .setBody().constant("health-check-consumer-ping")
             .process(exchange -> {
-                exchange.getOut().setHeaders(exchange.getIn().getHeaders());
                 ProducerHealthChecker producerHealthChecker = new ProducerHealthChecker();
                 exchange.getOut().setHeader("producer", producerHealthChecker.check() ? "UP" : "DOWN");
             });
+    }
+
+    class HeadersAggregationStrategy implements AggregationStrategy{
+
+        @Override
+        public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+            if (Objects.isNull(oldExchange))
+                return newExchange;
+            oldExchange.getIn().getHeaders().putAll(newExchange.getIn().getHeaders());
+            return oldExchange;
+
+        }
     }
 }
