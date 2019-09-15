0bd7e0917d663c498ae3290f71f8b1799999dc12
marceloserpa <jdev.marcelo@gmail.com>
2019-09-14 21:05:34 -0300

    spring webflux

---
+++ spring-webflux-gs/src/main/java/com/marceloserpa/spring/webflux/gs/Application.java
@@ -0,0 +1,14 @@
+package com.marceloserpa.spring.webflux.gs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	
+}
