72dc065a991aa206c1e1c08cc90ee345e8b05e46
marceloserpa <jdev.marcelo@gmail.com>
2021-10-22 07:36:46 -0300

    spring-config: creating config server

---
+++ spring-config/configserver/src/main/java/com/marceloserpa/configserver/ConfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.marceloserpa.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverApplication.class, args);
+	}
+
+}
---
+++ spring-config/configserver/src/test/java/com/marceloserpa/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marceloserpa.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
