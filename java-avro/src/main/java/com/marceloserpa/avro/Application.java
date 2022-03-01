4aee5e7970c929fb4a9c94ca75651f8e6705384b
marceloserpa <jdev.marcelo@gmail.com>
2022-03-01 15:18:11 -0300

    avro + java

---
+++ java-avro/src/main/java/com/marceloserpa/avro/Application.java
@@ -0,0 +1,42 @@
+package com.marceloserpa.avro;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecordBuilder;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+
+        Schema.Parser parser = new Schema.Parser();
+        Schema schema = parser.parse("{\n" +
+                "     \"type\": \"record\",\n" +
+                "     \"namespace\": \"com.example\",\n" +
+                "     \"name\": \"Customer\",\n" +
+                "     \"fields\": [\n" +
+                "       { \"name\": \"first_name\", \"type\": \"string\", \"doc\": \"First Name of Customer\" },\n" +
+                "       { \"name\": \"last_name\", \"type\": \"string\", \"doc\": \"Last Name of Customer\" },\n" +
+                "       { \"name\": \"age\", \"type\": \"int\", \"doc\": \"Age at the time of registration\" },\n" +
+                "       { \"name\": \"height\", \"type\": \"float\", \"doc\": \"Height at the time of registration in cm\" },\n" +
+                "       { \"name\": \"weight\", \"type\": \"float\", \"doc\": \"Weight at the time of registration in kg\" },\n" +
+                "       { \"name\": \"automated_email\", \"type\": \"boolean\", \"default\": true, \"doc\": \"Field indicating if the user is enrolled in marketing emails\" }\n" +
+                "     ]\n" +
+                "}");
+
+        GenericRecordBuilder customerBuilder = new GenericRecordBuilder(schema);
+        customerBuilder.set("first_name", "John");
+        customerBuilder.set("last_name", "Doe");
+        customerBuilder.set("age", 25);
+        customerBuilder.set("height", 170f);
+        customerBuilder.set("weight", 80.5f);
+        customerBuilder.set("automated_email", false);
+        GenericData.Record customer = customerBuilder.build();
+
+
+        System.out.println(customer);
+
+
+    }
+
+}
