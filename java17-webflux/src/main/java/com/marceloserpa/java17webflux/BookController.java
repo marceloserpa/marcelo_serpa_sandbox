a3c052384c5a2b8bf394fb460d900e953c601df3
marceloserpa <jdev.marcelo@gmail.com>
2022-03-10 20:27:26 -0300

    spring webflux + java 17: adding Post endpoint

--- java17-webflux/src/main/java/com/marceloserpa/java17webflux/BookController.java
+++ java17-webflux/src/main/java/com/marceloserpa/java17webflux/BookController.java
@@ -1,10 +1,16 @@
 package com.marceloserpa.java17webflux;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
+@RequestMapping("/books")
 public class BookController {
 
     private BookService bookService;
@@ -13,10 +19,18 @@ public class BookController {
         this.bookService = bookService;
     }
 
-    @GetMapping("/books")
+    @GetMapping
     public Flux<Book> getAll(){
         return bookService.getAll().map(bookEntity -> {
             return new Book(bookEntity.id(), bookEntity.title(), bookEntity.author(), bookEntity.price());
+        });
+    }
+
+    @PostMapping
+    public Mono<Book> save(@RequestBody Book book){
+        var bookEntity = new BookEntity(book.id(), book.title(), book.author(), book.price());
+        return bookService.save(bookEntity).map(entity -> {
+            return new Book(entity.id(), entity.title(), entity.author(), entity.price());
         });
     }
 
--- java17-webflux/src/main/java/com/marceloserpa/java17webflux/BookService.java
+++ java17-webflux/src/main/java/com/marceloserpa/java17webflux/BookService.java
@@ -2,6 +2,7 @@ package com.marceloserpa.java17webflux;
 
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class BookService {
@@ -14,5 +15,9 @@ public class BookService {
 
     public Flux<BookEntity> getAll(){
         return bookRepository.findAll();
+    }
+
+    public Mono<BookEntity> save(BookEntity book) {
+        return bookRepository.save(book);
     }
 }
