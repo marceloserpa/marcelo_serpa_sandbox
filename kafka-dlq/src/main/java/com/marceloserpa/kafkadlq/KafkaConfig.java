2c0f0524c2acd04de263e22215828684956f47cd
marceloserpa <jdev.marcelo@gmail.com>
2022-06-01 20:15:25 -0300

    spring-kafka: clean up

--- kafka-dlq/src/main/java/com/marceloserpa/kafkadlq/KafkaConfig.java
+++ kafka-dlq/src/main/java/com/marceloserpa/kafkadlq/KafkaConfig.java
@@ -2,17 +2,11 @@ package com.marceloserpa.kafkadlq;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.RetryableTopic;
-import org.springframework.kafka.core.KafkaOperations;
-import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
-import org.springframework.kafka.listener.SeekToCurrentErrorHandler;
 import org.springframework.retry.annotation.Backoff;
-import org.springframework.util.backoff.FixedBackOff;
 
 
 @Configuration
