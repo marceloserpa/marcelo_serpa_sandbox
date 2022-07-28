af60e1b1c1d495c8529e191930b02de9cf31082c
marceloserpa <jdev.marcelo@gmail.com>
2022-07-28 00:20:14 -0300

    ldap jni: lookup user on LDAP with admin user

--- java-ldap-pure/src/test/java/com/mserpa/ldap/LdapTest.java
+++ java-ldap-pure/src/test/java/com/mserpa/ldap/LdapTest.java
@@ -13,9 +13,12 @@ import org.junit.runner.RunWith;
 
 import javax.naming.AuthenticationException;
 import javax.naming.Context;
-import javax.naming.directory.DirContext;
-import javax.naming.directory.InitialDirContext;
+import javax.naming.NamingEnumeration;
+import javax.naming.directory.*;
 import java.util.Hashtable;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(FrameworkRunner.class)
 @CreateLdapServer(transports = { @CreateTransport(protocol = "LDAP", address = "localhost", port = 10390)})
@@ -50,6 +53,45 @@ public class LdapTest extends AbstractLdapTestUnit {
 
         DirContext context = new InitialDirContext(environment);
         context.close();
+    }
+
+    @Test
+    public void testLookUpDNUserWithAdminUser() throws Exception {
+        // setup admin context
+        Hashtable<String, String> environment = new Hashtable<String, String>();
+        environment.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        environment.put(Context.PROVIDER_URL, "ldap://localhost:10390");
+        environment.put(Context.SECURITY_AUTHENTICATION, "simple");
+
+        environment.put(Context.SECURITY_PRINCIPAL, "uid=admin,ou=system");
+        environment.put(Context.SECURITY_CREDENTIALS, "secret");
+
+        DirContext adminContext = new InitialDirContext(environment);
+
+        // prepare filters for user Marcelo Serpa
+        String filter = "(&(objectClass=person)(cn=Marcelo Serpa))";
+
+        String[] attrIDs = { "cn" };
+        SearchControls searchControls = new SearchControls();
+        searchControls.setReturningAttributes(attrIDs);
+        searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+
+        // lookup user on LDAP
+        NamingEnumeration<SearchResult> searchResults = adminContext.search("dc=mserpa,dc=com", filter, searchControls);
+
+        assertTrue(searchResults.hasMore());
+
+        SearchResult result = (SearchResult) searchResults.next();
+        Attributes attrs = result.getAttributes();
+
+        String distinguishedName = result.getNameInNamespace();
+        assertEquals(distinguishedName, "cn=Marcelo Serpa,ou=Users,dc=mserpa,dc=com");
+
+        String commonName = attrs.get("cn").toString();
+        assertEquals(commonName, "cn: Marcelo Serpa");
+
+
+        adminContext.close();
     }
 
 }
