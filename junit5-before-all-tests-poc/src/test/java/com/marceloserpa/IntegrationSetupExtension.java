bd846e59fc4a9091760f8c32007aa336048494c7
marceloserpa <jdev.marcelo@gmail.com>
2022-12-01 13:19:44 -0300

    junit 5: fix race condition issues

--- junit5-before-all-tests-poc/src/test/java/com/marceloserpa/IntegrationSetupExtension.java
+++ junit5-before-all-tests-poc/src/test/java/com/marceloserpa/IntegrationSetupExtension.java
@@ -3,20 +3,30 @@ package com.marceloserpa;
 
 import org.junit.jupiter.api.extension.BeforeAllCallback;
         import org.junit.jupiter.api.extension.ExtensionContext;
-        import static org.junit.jupiter.api.extension.ExtensionContext.Namespace.GLOBAL;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static org.junit.jupiter.api.extension.ExtensionContext.Namespace.GLOBAL;
 
 public class IntegrationSetupExtension implements BeforeAllCallback, ExtensionContext.Store.CloseableResource {
 
-    private static boolean started = false;
+    private static final Lock LOCK = new ReentrantLock();
+    private static volatile boolean started = false;
 
     @Override
     public void beforeAll(ExtensionContext context) {
-        if (!started) {
-            started = true;
-            // Your "before all tests" startup logic goes here
-            // The following line registers a callback hook when the root test context is shut down
-            context.getRoot().getStore(GLOBAL).put("any unique name", this);
-            new FakeDB().save(new Book(1L, "The Shining", "Stephen King"));
+        LOCK.lock();
+        try {
+            if (!started) {
+                started = true;
+                // Your "before all tests" startup logic goes here
+                // The following line registers a callback hook when the root test context is shut down
+                context.getRoot().getStore(GLOBAL).put("any unique name", this);
+                new FakeDB().save(new Book(1L, "The Shining", "Stephen King"));
+            }
+        } finally {
+            LOCK.unlock();
         }
     }
 
