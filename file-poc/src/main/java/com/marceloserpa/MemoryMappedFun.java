cccd6a86ddaeff72ce2a8c216a44727c5b3514af
marceloserpa <jdev.marcelo@gmail.com>
2024-01-13 20:04:53 -0300

    Identity the end of line

--- file-poc/src/main/java/com/marceloserpa/MemoryMappedFun.java
+++ file-poc/src/main/java/com/marceloserpa/MemoryMappedFun.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MemoryMappedFun {
     private static final String FILE = "./metrics.txt";
@@ -20,9 +22,26 @@ public class MemoryMappedFun {
                         channel.size());
 
         System.out.println("Reading content and printing ... ");
+
+        char a;
+        List<String> lines = new ArrayList<>();
+        StringBuffer sb = new StringBuffer();
         for (int i = 0; i < channel.size(); i++) {
-            System.out.print((char) mbb.get());
+            a = (char) mbb.get();
+            if( a == '\n') {
+                lines.add(sb.toString());
+
+                sb =  new StringBuffer();
+            }
+
+            sb.append(a);
+            System.out.print(a);
         }
+
+        System.out.println("--------------");
+
+        System.out.println(lines);
+
 
         channel.close();
         file.close();
