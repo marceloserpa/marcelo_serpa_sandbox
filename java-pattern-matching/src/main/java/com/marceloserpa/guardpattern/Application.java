ba0b3edcd5e6c43870e9ddff06b289d0353bcb7b
marceloserpa <jdev.marcelo@gmail.com>
2024-02-23 08:32:35 -0300

    java21: add guard to pattern matching

--- java-pattern-matching/src/main/java/com/marceloserpa/guardpattern/Application.java
+++ java-pattern-matching/src/main/java/com/marceloserpa/guardpattern/Application.java
@@ -6,14 +6,11 @@ public class Application {
     public static void main(String[] args) {
 
         System.out.println(damageCalc(new AirPollution()));
-
-
     }
-
 
     public static int damageCalc(Object o){
         return switch (o) {
-            case AirPollution airPollution -> airPollution.getAQI();
+            case AirPollution airPollution when airPollution.getAQI() > 200 -> 500;
             case Discrimination discrimination -> discrimination.damagingGenerations();
             case Deforestation deforestation -> deforestation.getTreeDamage();
             case null, default -> -1;
