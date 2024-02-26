96b74f6d2b3b8c93b6238f9c2ccda32765c02e67
marceloserpa <jdev.marcelo@gmail.com>
2024-02-26 07:52:49 -0300

    java: fun with destructors

--- java-pattern-matching/src/main/java/com/marceloserpa/instanceofclean/RecordDestructorFun.java
+++ java-pattern-matching/src/main/java/com/marceloserpa/instanceofclean/RecordDestructorFun.java
@@ -1,2 +1,47 @@
-package com.marceloserpa.instanceofclean;public class RecordDestructorFun {
+package com.marceloserpa.instanceofclean;
+
+public class RecordDestructorFun {
+
+    record Point(int x, int y){};
+
+    static class DestructorPoC {
+
+        void printPointLegacy(Object object) {
+            if(object instanceof Point) {
+                Point point = (Point) object;
+                int x = point.x;
+                int y = point.y;
+                System.out.printf("X is %d, y is %d%n", x, y);
+            }
+        }
+
+        void printPointJava16(Object object) {
+            if(object instanceof Point point) {
+                int x = point.x;
+                int y = point.y;
+                System.out.printf("X is %d, y is %d%n", x, y);
+            }
+
+        }
+
+        void printPointJava21(Object object) {
+            if(object instanceof Point(int x, int y)) {
+                System.out.printf("X is %d, y is %d%n", x, y);
+            }
+
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+        var poc = new DestructorPoC();
+
+        var point = new Point(1,2);
+        poc.printPointLegacy(point);
+        poc.printPointJava16(point);
+        poc.printPointJava21(point);
+
+    }
 }
