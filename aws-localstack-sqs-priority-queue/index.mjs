4d2a1cd01ddd773e16a414b9225ddf2f5b43c6d7
marceloserpa <jdev.marcelo@gmail.com>
2024-03-09 18:38:55 -0300

    sqs priority wip

--- aws-localstack-sqs-priority-queue/index.mjs
+++ aws-localstack-sqs-priority-queue/index.mjs
@@ -1,5 +1,33 @@
 export const handler = async (event) => {
+	/*
 	const messages = event.Records.map(record => record.body);
-	messages.forEach(message => console.log(message));
+	
+	messages.forEach(message => {
+		console.log(message);
+	});
+	return messages;
+	*/
+
+	console.log("++++++++++++++++++==")
+
+	const mainQueue = "my-messages-main";
+	const priorityQueue = "my-messages-priority";
+
+	const messages = event.Records.map(record => record.body);
+
+	for(var i=0; i<event.Records.length;i++){
+
+		console.log("processing")
+		var queue = event.Records[i].eventSourceARN.split(':').pop() 
+		if(queue === mainQueue) {
+			console.log('main --')
+		} else if (queue === priorityQueue) {
+			console.log('priority --')
+		} else {
+			console.log('unknown')
+		}
+
+	}
+
 	return messages;
 };
