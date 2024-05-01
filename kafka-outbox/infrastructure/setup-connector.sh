212bd8debf0d09bce9676fff9c1ec54eb5dc9637
marceloserpa <jdev.marcelo@gmail.com>
2024-05-01 08:51:22 -0300

    outbox-poc: simulate sharding database

--- kafka-outbox/infrastructure/setup-connector.sh
+++ kafka-outbox/infrastructure/setup-connector.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 curl -X POST --location "http://localhost:8083/connectors" -H "Content-Type: application/json" -H "Accept: application/json" -d @database01-source-connector.json
+
+
+
+curl -X POST --location "http://localhost:8083/connectors" -H "Content-Type: application/json" -H "Accept: application/json" -d @database02-source-connector.json
