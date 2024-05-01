19ed604e742b803046befc805ad78be3ab5da7ea
marceloserpa <jdev.marcelo@gmail.com>
2024-05-01 15:32:30 -0300

    outbox-poc: refactoring

--- kafka-outbox/multi-tenant-outbox/src/main/java/com/marceloserpa/multitenantoutbox/user/UserRepository.java
+++ kafka-outbox/multi-tenant-outbox/src/main/java/com/marceloserpa/multitenantoutbox/user/UserRepository.java
@@ -3,4 +3,5 @@ package com.marceloserpa.multitenantoutbox.user;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+    Iterable<User> findByTenantId(Long tenantId);
 }
