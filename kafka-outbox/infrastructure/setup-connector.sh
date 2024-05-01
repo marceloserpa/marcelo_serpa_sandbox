f646793d5ebc3a067a8b51aaad33e6f74c568345
marceloserpa <jdev.marcelo@gmail.com>
2024-05-01 09:10:02 -0300

    outbox-poc: refactoring structure

--- kafka-outbox/infrastructure/setup-connector.sh
+++ kafka-outbox/infrastructure/setup-connector.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-curl -X POST --location "http://localhost:8083/connectors" -H "Content-Type: application/json" -H "Accept: application/json" -d @database01-source-connector.json
+connectors=("database01-source-connector" "database02-source-connector")
+#connectors=("database01-source-connector")
 
+for connector in ${connectors[@]}; do
+    echo ">> Starting $connector setup"
+    curl -X POST --location "http://localhost:8083/connectors" \
+        -H "Content-Type: application/json" \
+        -H "Accept: application/json" \
+        -d @$connector.json
 
-
-curl -X POST --location "http://localhost:8083/connectors" -H "Content-Type: application/json" -H "Accept: application/json" -d @database02-source-connector.json
+    sleep 2
+    echo ">> Completed $connector setup"
+done
