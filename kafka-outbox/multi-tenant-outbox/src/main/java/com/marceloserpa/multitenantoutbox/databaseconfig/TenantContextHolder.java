ad79a0bc9b9aac1833cda2a19ccf9129f55fd362
marceloserpa <jdev.marcelo@gmail.com>
2024-05-01 12:11:30 -0300

    outbox-poc: use dynamic routing to simulate multi-tenancy

---
+++ kafka-outbox/multi-tenant-outbox/src/main/java/com/marceloserpa/multitenantoutbox/databaseconfig/TenantContextHolder.java
@@ -0,0 +1,20 @@
+package com.marceloserpa.multitenantoutbox.databaseconfig;
+
+public class TenantContextHolder {
+
+    private static final ThreadLocal<TenantDatabase> contextHolder =
+            new ThreadLocal<TenantDatabase>();
+
+    public static void setDatabase(TenantDatabase tenantDatabase) {
+        contextHolder.set(tenantDatabase);
+    }
+
+    public static TenantDatabase getTenantDatabase() {
+        return (TenantDatabase) contextHolder.get();
+    }
+
+    public static void clearDatabase() {
+        contextHolder.remove();
+    }
+
+}
