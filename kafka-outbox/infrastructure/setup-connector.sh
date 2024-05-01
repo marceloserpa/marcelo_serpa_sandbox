d7971e877f990b8917e4c077dead3431f4485b08
marceloserpa <jdev.marcelo@gmail.com>
2024-05-01 08:36:50 -0300

    outbox-poc: refactoring

--- kafka-outbox/infrastructure/setup-connector.sh
+++ kafka-outbox/infrastructure/setup-connector.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -X POST --location "http://localhost:8083/connectors" -H "Content-Type: application/json" -H "Accept: application/json" -d @connector.json
+curl -X POST --location "http://localhost:8083/connectors" -H "Content-Type: application/json" -H "Accept: application/json" -d @database01-source-connector.json
