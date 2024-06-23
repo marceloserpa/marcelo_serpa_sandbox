6ef46cc28f70bf1882bfa7ad4827eef13b0d7f11
marceloserpa <jdev.marcelo@gmail.com>
2024-06-23 14:34:40 -0300

    kms: refactoring

--- aws-kms/src/main/java/com/mserpa/kmsfun/Main.java
+++ aws-kms/src/main/java/com/mserpa/kmsfun/Main.java
@@ -2,14 +2,11 @@ package com.mserpa.kmsfun;
 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.kms.KmsClient;
-import software.amazon.awssdk.services.kms.model.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
 
 public class Main {
 
@@ -31,11 +28,13 @@ public class Main {
                 .endpointOverride(new URI(ENDPOINT_URL))
                 .build();
 
-        var encrypted = encrypt(kmsClient, keyId, sensibledata);
+        var kmsCrypto = new KmsCrypto();
+
+        var encrypted = kmsCrypto.encrypt(kmsClient, keyId, sensibledata);
 
         System.out.println("ENCRYPTED: " + encrypted);
 
-        var decrypted = decrypt(kmsClient, keyId, encrypted);
+        var decrypted = kmsCrypto.decrypt(kmsClient, keyId, encrypted);
 
         System.out.println("DECRYPTED: " + decrypted);
 
@@ -47,47 +46,6 @@ public class Main {
 
     }
 
-    public static SdkBytes encrypt(KmsClient kmsClient, String keyId, String plainText){
-        try {
-            SdkBytes myBytes = SdkBytes.fromUtf8String(plainText);
-            EncryptRequest encryptRequest = EncryptRequest.builder()
-                    .keyId(keyId)
-                    .plaintext(myBytes)
-                    .encryptionAlgorithm(EncryptionAlgorithmSpec.SYMMETRIC_DEFAULT)
-                    .build();
-
-            EncryptResponse response = kmsClient.encrypt(encryptRequest);
-            String algorithm = response.encryptionAlgorithm().toString();
-            System.out.println("The string was encrypted with algorithm " + algorithm + ".");
-
-            //SdkBytes encryptedData = response.ciphertextBlob();
-
-            //return Base64.getEncoder().encodeToString(encryptedData.asByteArray());
-            return response.ciphertextBlob();
-
-        } catch (KmsException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    public static String decrypt(KmsClient kmsClient, String keyId, SdkBytes encryptedText){
-        try {
-            DecryptRequest decryptRequest = DecryptRequest.builder()
-                    .ciphertextBlob(encryptedText)
-                    .keyId(keyId)
-                    .build();
-
-            DecryptResponse decryptResponse = kmsClient.decrypt(decryptRequest);
-            return decryptResponse.plaintext().asString(StandardCharsets.UTF_8);
-
-        } catch (KmsException e) {
-            e.printStackTrace();
-        }
-        return "";
-
-
-    }
 
 
 }
