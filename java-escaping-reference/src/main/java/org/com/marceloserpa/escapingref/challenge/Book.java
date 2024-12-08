769432c61304a8c189363f32108648b05959f33a
marceloserpa <jdev.marcelo@gmail.com>
2024-12-08 13:22:14 -0300

    escaping reference challenge

---
+++ java-escaping-reference/src/main/java/org/com/marceloserpa/escapingref/challenge/Book.java
@@ -0,0 +1,39 @@
+package org.com.marceloserpa.escapingref.challenge;
+
+public class Book {
+	private int id;
+	private String title;
+	private String author;
+	private Price price;
+	
+	public int getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public Book(int id, String title, String author, Double price) {
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.price = new Price(price);
+	}
+	
+	public String toString() {
+		return title + " by " + author;
+	}
+	
+	public Price getPrice() {
+		return this.price;
+	}
+	
+	public void setPrice(Double price) {
+		this.price = new Price(price);
+	}
+}
