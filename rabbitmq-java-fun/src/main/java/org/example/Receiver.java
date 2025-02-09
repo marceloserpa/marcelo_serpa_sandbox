2ecdf5085240e16cfe35ff80e29176aaedada2da
marceloserpa <jdev.marcelo@gmail.com>
2025-02-09 08:17:23 -0300

    fun with rabbitmq: receive message

--- rabbitmq-java-fun/src/main/java/org/example/Receiver.java
+++ rabbitmq-java-fun/src/main/java/org/example/Receiver.java
@@ -1,13 +1,14 @@
 package org.example;
 
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
-public class Main {
+public class Receiver {
 
     private final static String QUEUE_NAME = "test-queue";
 
@@ -18,9 +19,13 @@ public class Main {
              Channel channel = connection.createChannel()) {
 
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            String message = "Hello World!";
-            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-            System.out.println(" [x] Sent '" + message + "'");
+
+
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+            };
+            channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
 
         }
 
