ec85c74887d3c654d7699240feb293aeca76e9a0
marceloserpa <jdev.marcelo@gmail.com>
2025-02-09 08:57:51 -0300

    spring + rabbit fun

---
+++ spring-rabbit-poc/src/main/java/com/marceloserpa/springrabbitfun/SpringrabbitfunApplication.java
@@ -0,0 +1,13 @@
+package com.marceloserpa.springrabbitfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrabbitfunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrabbitfunApplication.class, args);
+	}
+
+}
---
+++ spring-rabbit-poc/src/test/java/com/marceloserpa/springrabbitfun/SpringrabbitfunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marceloserpa.springrabbitfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrabbitfunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
