7e706dc793e7090d5b004ba91e997d3d2997940e
marceloserpa <jdev.marcelo@gmail.com>
2025-02-09 08:11:18 -0300

    fun with rabbitmq

---
+++ rabbitmq-java-fun/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.rabbitmq:amqp-client:5.25.0")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ rabbitmq-java-fun/settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "rabbitmq-java-fun"
+
