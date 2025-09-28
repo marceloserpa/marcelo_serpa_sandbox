933b4f3dc4c16229b0c84e700cdba754bb0c4956
marceloserpa <jdev.marcelo@gmail.com>
2025-09-28 09:35:49 -0300

    SpringBoot: Redis integration

---
+++ spring-redis/src/test/java/com/marceloserpa/springredis/SpringredisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marceloserpa.springredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringredisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
