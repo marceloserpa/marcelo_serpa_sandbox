c348e0b51ae58f20f171a0baae8fa32f5a7051dc
marceloserpa <jdev.marcelo@gmail.com>
2026-01-28 09:49:39 -0300

    oop: enum types way

---
+++ enum-vs-records/src/main/java/com/marceloserpa/enumxrecords/Main.java
@@ -0,0 +1,9 @@
+package com.marceloserpa.enumxrecords;
+
+public class Main {
+
+    static void main() {
+         var payment = new Payment(Type.CREDIT_CARD, "1234560", "741258", "2020");
+        System.out.println(payment);
+    }
+}
---
+++ enum-vs-records/src/main/java/com/marceloserpa/enumxrecords/Type.java
@@ -0,0 +1,6 @@
+package com.marceloserpa.enumxrecords;
+
+public enum Type {
+
+    CREDIT_CARD, WIRE_TRANSFER
+}
