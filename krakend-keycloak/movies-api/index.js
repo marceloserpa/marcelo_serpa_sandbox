49485c179d1ffe5e5e10c4e9eef340afdc8aaa64
marceloserpa <jdev.marcelo@gmail.com>
2026-02-10 09:38:46 -0300

    KrakenD + KeyCloak: Setup Krakend and backend API

--- krakend-keycloak/movies-api/index.js
+++ krakend-keycloak/movies-api/index.js
@@ -2,33 +2,7 @@ import express from "express";
 
 const app = express();
 
-import { createRemoteJWKSet, jwtVerify } from "jose";
-
-const JWKS = createRemoteJWKSet(
-    new URL(
-      "http://localhost:8080/realms/marcelocorp/protocol/openid-connect/certs"
-    )
-  );
-
 const port = 3000;
-
-const authInterceptor = async function (req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-
-    if (token == null) {
-      return res.status(401).send('Authentication token required.'); 
-    }
-  
-    const { payload } = await jwtVerify(token, JWKS, {
-        issuer: "http://localhost:8080/realms/marcelocorp"
-      });
-  
-      req.user = payload;
-    next()
-}
-
-app.use(authInterceptor)
  
 app.get('/movie/v1/movies/', (req, res) =>  {
     const movies = [
--- krakend-keycloak/scripts/movies-api-request.sh
+++ krakend-keycloak/scripts/movies-api-request.sh
@@ -1,5 +1,5 @@
 TOKEN=$1
 
 curl -X GET \
-    "http://localhost:3000/movie/v1/movies" \
+    "http://localhost:9000/api/movie/v1/movies" \
     -H "Authorization: Bearer $TOKEN" 
