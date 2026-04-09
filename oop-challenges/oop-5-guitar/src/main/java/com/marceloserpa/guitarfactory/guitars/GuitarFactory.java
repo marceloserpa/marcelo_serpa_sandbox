e8d0cd75ae6ad98f27588eba3d15916b58740417
marceloserpa <jdev.marcelo@gmail.com>
2026-04-09 08:51:33 -0300

    OOP 5: Guitar Challenge

--- oop-challenges/oop-5-guitar/src/main/java/com/marceloserpa/guitarfactory/guitars/GuitarFactory.java
+++ oop-challenges/oop-5-guitar/src/main/java/com/marceloserpa/guitarfactory/guitars/GuitarFactory.java
@@ -1,16 +1,31 @@
 package com.marceloserpa.guitarfactory.guitars;
 
 import com.marceloserpa.guitarfactory.components.GuitarSpec;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class GuitarFactory {
 
-    public enum TYPE { ELECTRIC, ACOUSTIC, BASS}
+    public enum TYPE {
+        ELECTRIC("EL"),
+        ACOUSTIC("AC"),
+        BASS("BA");
+
+        private final String prefix;
+        TYPE(String prefix) { this.prefix = prefix; }
+        public String prefix() { return prefix; }
+    }
+
+    private final AtomicInteger counter = new AtomicInteger(1);
 
     public Guitar create(TYPE type, GuitarSpec spec, String model) {
         return switch (type) {
-            case ELECTRIC -> new ElectricGuitar("A", spec, model);
-            case ACOUSTIC -> new AcousticGuitar("B", spec, model);
-            case BASS -> new BassGuitar("C", spec, model);
+            case ELECTRIC -> new ElectricGuitar(generateSerial(type), spec, model);
+            case ACOUSTIC -> new AcousticGuitar(generateSerial(type), spec, model);
+            case BASS     -> new BassGuitar(generateSerial(type), spec, model);
         };
+    }
+
+    private String generateSerial(TYPE type) {
+        return "%s-%03d".formatted(type.prefix(), counter.getAndIncrement());
     }
 }
