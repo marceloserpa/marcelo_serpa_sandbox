9b61eaad77f9eb10b139d430045ee761c3053131
marceloserpa <jdev.marcelo@gmail.com>
2026-06-03 08:40:44 -0300

    outbox-debezium: removing unused imports

--- debezium-chaos-experiment/debezium-outbox-poc/src/main/java/com/marceloserpa/debezium/outbox/user/UserService.java
+++ debezium-chaos-experiment/debezium-outbox-poc/src/main/java/com/marceloserpa/debezium/outbox/user/UserService.java
@@ -1,16 +1,12 @@
 package com.marceloserpa.debezium.outbox.user;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.marceloserpa.debezium.outbox.outbox.OutboxService;
-import com.marceloserpa.debezium.outbox.outbox.Payload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class UserService {
