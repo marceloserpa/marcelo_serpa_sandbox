358eb852cd7e3487754c65a3fcac544de1cf5cab
marceloserpa <jdev.marcelo@gmail.com>
2026-06-28 09:36:22 -0300

    Chaos Testing PoC: Improve code

--- toxic-proxy-poc/src/main/java/com/marceloserpa/toxicproxypoc/CharacterService.java
+++ toxic-proxy-poc/src/main/java/com/marceloserpa/toxicproxypoc/CharacterService.java
@@ -1,13 +1,11 @@
 package com.marceloserpa.toxicproxypoc;
 
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.types.Expiration;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import tools.jackson.databind.ObjectMapper;
 
 import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 
 @Service
@@ -24,24 +22,26 @@ public class CharacterService {
     }
 
     public Optional<Character> findById(Long id) {
-
-        String characterCache = redisTemplate.opsForValue().get("starwars:people:" + id);
-
-        if(characterCache == null) {
-            IO.println("Not in cache ...");
-            Character character = restClient.get().uri("people/"+id+"/")
-                    .retrieve()
-                    .body(Character.class);
-            IO.println(character);
-            if(character != null) {
-                var value = objectMapper.writeValueAsString(character);
-                redisTemplate.opsForValue().set("starwars:people:" + id, value, Expiration.from(Duration.of(30, ChronoUnit.SECONDS)));
-                IO.println("Character id=" + id + " cached.");
-                return Optional.of(character);
-            }
+        String key = "starwars:people:" + id;
+        String cached = redisTemplate.opsForValue().get(key);
+        if(cached != null) {
+            return Optional.of(objectMapper.readValue(cached, Character.class));
         }
-        var character = objectMapper.readValue(characterCache, Character.class);
-        return Optional.ofNullable(character);
+
+        IO.println("Not in cache ...");
+        Character character = restClient.get().uri("people/"+id+"/")
+                .retrieve()
+                .body(Character.class);
+        if(character == null) {
+            return Optional.empty();
+        }
+
+        redisTemplate.opsForValue().set(key,
+                objectMapper.writeValueAsString(character),
+                Duration.ofSeconds(30));
+        IO.println("Character id=" + id + " cached.");
+
+        return Optional.of(character);
     }
 
 }
